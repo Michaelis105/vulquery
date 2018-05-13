c19ccd8463505f8c142434178b6ad2b6d5f0d7e3
Michael Louie <michael.louie10@gmail.com>
2018-05-13 18:33:21 -0400

    Implemented configuration and database initialization skeleton

--- src/main/java/xyz/vulquery/Application.java
+++ src/main/java/xyz/vulquery/Application.java
@@ -1,12 +1,62 @@
 package xyz.vulquery;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import xyz.vulquery.util.StringUtils;
+
+import javax.annotation.PostConstruct;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @SpringBootApplication
 public class Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    private ConfigProperties prop;
+
+    /**
+     * Creates database if it does not exists.
+     * @throws ClassNotFoundException internal error in dependencies
+     * @throws SQLException connection error to database
+     * @throws URISyntaxException parsing path error
+     */
+    @PostConstruct
+    private void initializeDataStore() throws ClassNotFoundException, SQLException, URISyntaxException {
+        Class.forName("org.sqlite.JDBC");
+        StringBuffer urlSB = new StringBuffer();
+
+        urlSB.append("jdbc:sqlite:");
+        urlSB.append(StringUtils.isBlank(prop.getDbpath()) ?
+                Application.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath() : prop.getDbpath());
+        urlSB.append("vulquery.db");
+
+        logger.debug(urlSB.toString());
+
+        // TODO: Not final table
+        String sql = "CREATE TABLE DEPENDENCY " +
+                "(GROUPID       TEXT    NOT NULL, " +
+                " ARTIFACTID    TEXT    NOT NULL, " +
+                " VERSION       TEXT    NOT NULL)";
+
+        try (Connection connection = DriverManager.getConnection(urlSB.toString());
+             Statement statement = connection.createStatement()
+        ) {
+            statement.executeUpdate(sql);
+        }
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
---
+++ src/main/java/xyz/vulquery/ConfigProperties.java
@@ -0,0 +1,33 @@
+package xyz.vulquery;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+//@PropertySource("classpath:application.properties")
+@Configuration
+@ConfigurationProperties(prefix = "vq")
+public class ConfigProperties {
+    private String dbpath;
+    private String datafeedpath;
+
+    @Override
+    public String toString() {
+        return "ConfigProperties [ dbpath=" + dbpath +  " , datafeedpath=" + datafeedpath + " ]";
+    }
+
+    public void setDbpath(String dbpath) {
+        this.dbpath = dbpath;
+    }
+
+    public String getDbpath() {
+        return dbpath;
+    }
+
+    public void setDatafeedpath(String datafeedpath) {
+        this.datafeedpath = datafeedpath;
+    }
+
+    public String getDatafeedpath() {
+        return datafeedpath;
+    }
+}
